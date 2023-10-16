f1d0cedd859637de4c58b97e254a0fd15d3a08f6
Georgia Wright <georgiawright617@gmail.com>
2023-10-16 17:30:51 +0100

    week 4 mon

--- week_4/activity_1_libraries.py
+++ week_4/activity_1_libraries.py
@@ -1,10 +1,37 @@
+
+#use date time library
+
+# todays date
+
+# date born
+
+
+
+
 import datetime
 
-date = datetime.datetime(2020, 5, 17)
+# first attempt
+# date = datetime.datetime(2020, 5, 17)
 
-number_of_days_in_year = date.strftime("%j"))
+# number_of_days_in_year = date.strftime("%j"))
 
-number_of_days_in_year + (365*)
+# number_of_days_in_year + (365*)
+
+today = datetime.date.today()
+
+dob = datetime.date(1991, 8, 13)
+
+alive_for = (today - dob).days
+
+print(f" I have been alive for {alive_for} days")
+
+
+
+
+
+
+
+
 
 
 
