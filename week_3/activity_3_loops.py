cc73f869f45213a2e87cd5b5a3629afa8cf117fb
Georgia Wright <georgiawright617@gmail.com>
2023-10-23 13:29:06 +0100

    edited the password activity

--- week_3/activity_3_loops.py
+++ week_3/activity_3_loops.py
@@ -14,27 +14,23 @@
 
 password = "fruitcake"
 
-user_entry1 = input("Enter your password   >   ")
+user_entry = input("Enter your password   >   ")
+attempts = 1
 
-while user_entry1 != password:
+while user_entry != password and attempts < 3:
     print("Try again")
-    user_entry2 = input("Enter your password!   >   ")
-    if user_entry1 != password and user_entry2 != password:
-        print("Try again!")
-        user_entry3 = input("Enter your password!!   >   ")
-        if user_entry1 != password and user_entry2 != password and user_entry3 != password:
-            print("Login failed.")
-            break
-        else:
-            print("Correct, you're logged in.")
-            break
-    else:
-        print("Correct, you have logged in successfully.")
-        break
-        
+    attempts += 1
+    user_entry = input("Enter your password!   >   ")
+    
 
 else:
-    print("Correct, you have logged in successfully.")
+        if user_entry == password:
+            print("You have logged in successfully.")
+        
+        else:
+            print("You have ran out of attempts.")
+
+
 
 
 
