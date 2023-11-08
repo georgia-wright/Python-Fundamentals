9546bddc41454ea7847f622b73718a1e4575ce06
Georgia Wright <georgiawright617@gmail.com>
2023-11-08 16:55:14 +0000

    addition to superhero

--- week_6/day_02/OOP_level_up.py
+++ week_6/day_02/OOP_level_up.py
@@ -12,9 +12,14 @@ lauryn = Superhero("Lauryn", "Red Fox", "Time Travel", "Dog")
 
 print(f"Lauryn has the Superhero Name of {lauryn.identity}")
 
+
 elle = Superhero("Elle", "Elfie", "Mind Reading", "Santa")
 
 print(f"Elle's arch enemy is {elle.enemy} and her Superhero name is {elle.identity}")
+
+elle.introduce()
+
+
 
 
 
--- week_6/day_02/superhero.py
+++ week_6/day_02/superhero.py
@@ -5,3 +5,14 @@ class Superhero():
         self.identity = superhero_identity
         self.power = super_power
         self.enemy = arch_enemy
+
+
+    def introduce(self):
+        print(f"My name is {self.name} but my Superhero name is {self.identity}.")
+        print(f"My superpower is {self.power} and my enemy is {self.enemy}")
+
+
+
+
+
+
